73bf9d3bc23496aa93f19b8023af89cb91867aad
Hsiaoming Yang <me@lepture.com>
2013-06-09 18:57:06 +0800

    more test cases

--- test_otpauth.py
+++ test_otpauth.py
@@ -1,4 +1,5 @@
 from otpauth import OtpAuth
+from nose.tools import raises
 
 
 def test_hotp():
@@ -6,8 +7,42 @@ def test_hotp():
     code = auth.hotp(4)
     assert auth.valid_hotp(code) == 4
 
+    # false
+    assert auth.valid_hotp(1234567) is False
+    assert auth.valid_hotp(123456) is False
+    assert auth.valid_hotp('123456') is False
+
 
 def test_totp():
     auth = OtpAuth('python')
     code = auth.totp()
     assert auth.valid_totp(code)
+
+    # false
+    assert auth.valid_hotp(1234567) is False
+    assert auth.valid_hotp(123456) is False
+
+
+@raises(TypeError)
+def test_to_google_raise():
+    auth = OtpAuth('python')
+    auth.to_google('invalid', 'python', 'python')
+
+
+@raises(ValueError)
+def test_to_google_hotp_raise():
+    auth = OtpAuth('python')
+    auth.to_google('hotp', 'python', 'python')
+
+
+def test_to_google_hotp():
+    auth = OtpAuth('python')
+    expect = ('otpauth://hotp/python?secret=OB4XI2DPNY======'
+              '&issuer=python&counter=4')
+    assert auth.to_google('hotp', 'python', 'python', 4) == expect
+
+
+def test_to_google_totp():
+    auth = OtpAuth('python')
+    expect = 'otpauth://totp/python?secret=OB4XI2DPNY======&issuer=python'
+    assert auth.to_google('totp', 'python', 'python') == expect
