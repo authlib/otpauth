dc779779b17a20f3ec607cdcb88505639b175b1b
Hsiaoming Yang <me@lepture.com>
2023-06-27 22:41:46 +0900

    chore: fix sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+html_static_path = ["_static"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
