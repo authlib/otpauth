0a4d5554ffd18806fb36163b452e18951a6dc895
Viicos <65306057+Viicos@users.noreply.github.com>
2023-07-20 09:49:26 +0200

    Add `mypy` to the project

--- src/otpauth/rfc6238.py
+++ src/otpauth/rfc6238.py
@@ -1,7 +1,9 @@
+import typing as t
 import time
 import hmac
 from .core import OTP
 from .rfc4226 import generate_hotp
+from .types import SupportedAlgorithms
 
 
 class TOTP(OTP):
@@ -13,13 +15,13 @@ class TOTP(OTP):
     :param period: The password valid in "period" seconds.
     """
 
-    TYPE = "TOTP"
+    TYPE: t.ClassVar[str] = "TOTP"
 
-    def __init__(self, secret: bytes, digit: int = 6, algorithm: str = "SHA1", period: int = 30):
+    def __init__(self, secret: bytes, digit: int = 6, algorithm: SupportedAlgorithms = "SHA1", period: int = 30):
         super().__init__(secret, digit, algorithm)
         self.period = period
 
-    def generate(self, timestamp: int = None) -> int:
+    def generate(self, timestamp: t.Optional[int] = None) -> int:
         """Generate a TOTP code. The returning result is an integer.
         To convert it into string with the correct digit length, developers
         can use :meth:`string_code`::
@@ -31,7 +33,7 @@ class TOTP(OTP):
         """
         return generate_totp(self.secret, self.period, timestamp, self.digit, self.algorithm)
 
-    def verify(self, code: int, timestamp: int = None) -> bool:
+    def verify(self, code: int, timestamp: t.Optional[int] = None) -> bool:  # type: ignore[override]
         """Valid a TOTP code for the given timestamp.
 
         :param code: A number to be verified.
@@ -41,7 +43,7 @@ class TOTP(OTP):
             return False
         return hmac.compare_digest(bytes(self.generate(timestamp)), bytes(code))
 
-    def to_uri(self, label: str, issuer: str) -> str:
+    def to_uri(self, label: str, issuer: str) -> str:  # type: ignore[override]
         """Generate the otpauth protocal string for TOTP.
 
         :param label: Label of the identifier.
@@ -51,7 +53,7 @@ class TOTP(OTP):
         return uri + f"&period={self.period}"
 
 
-def generate_totp(secret: bytes, period: int=30, timestamp: int=None, digit: int = 6, algorithm: str = "SHA1"):
+def generate_totp(secret: bytes, period: int = 30, timestamp: t.Optional[int] = None, digit: int = 6, algorithm: SupportedAlgorithms = "SHA1"):
     """Generate a TOTP code.
 
     A TOTP code is an extension of TOTP algorithm.
@@ -61,6 +63,6 @@ def generate_totp(secret: bytes, period: int=30, timestamp: int=None, digit: int
     :param timestamp: Create TOTP at this given timestamp, default is now.
     """
     if timestamp is None:
-        timestamp = time.time()
+        timestamp = int(time.time())
     counter = int(timestamp) // period
     return generate_hotp(secret, counter, digit, algorithm)
