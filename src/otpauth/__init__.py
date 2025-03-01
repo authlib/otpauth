9a29d6d8298b7d4f4721d0b791c3447b63df4577
Hsiaoming Yang <me@lepture.com>
2025-03-01 14:56:13 +0900

    chore: release 2.2.0

--- src/otpauth/__init__.py
+++ src/otpauth/__init__.py
@@ -1,12 +1,12 @@
-from ._rfc4226 import HOTP
-from ._rfc4226 import generate_hotp
-from ._rfc6238 import TOTP
-from ._rfc6238 import generate_totp
-from .core import SupportedAlgorithms
+from ._rfc4226 import HOTP as HOTP
+from ._rfc4226 import generate_hotp as generate_hotp
+from ._rfc6238 import TOTP as TOTP
+from ._rfc6238 import generate_totp as generate_totp
+from .core import SupportedAlgorithms as SupportedAlgorithms
 
 __author__ = "Hsiaoming Yang <me@lepture.com>"
 __homepage__ = "https://otp.authlib.org/"
-__version__ = "2.1.1"
+__version__ = "2.2.0"
 
 __all__ = [
     "SupportedAlgorithms",
